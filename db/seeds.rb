9be0a00be09ca1426112dd110d2b7af4c901e010
Kyle Davis <kyledavis@Kyles-iMac.local>
2021-05-05 21:34:11 -0600

    more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -96,19 +96,67 @@ employees = Employee.create([
     }
   },
   {
-    first_name: 'Sally',
-    last_name: 'Sue',
+    first_name: 'Mary',
+    last_name: 'May',
     title: 'Server',
-    email: 'sallu@gmail.com',
+    email: 'mary@gmail.com',
+    phone: '720-754-3847',
+    image: 'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2550&q=80',
+    hire_date: Date.parse('1/2/2019'),
+    training: false,
+    address: {
+      'state'=> 'Colorado',
+      'city'=> 'Denver',
+      'streeet_address' => '1234 Drive Ave',
+      'zip' => '50685'
+    }
+  },
+  {
+    first_name: 'David',
+    last_name: 'Simmons',
+    title: 'Bartender',
+    email: 'david@gmail.com',
     phone: '720-541-1290',
-    image: 'https://images.unsplash.com/photo-1618747946260-9511b46b1ac7?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzc4fHxwb3J0YWl0fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60',
-    hire_date: Date.parse('5/8/2019'),
+    image: 'https://images.unsplash.com/photo-1552058544-f2b08422138a?ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8cGVyc29ufGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60',
+    hire_date: Date.parse('6/8/2015'),
+    training: false,
+    address: {
+      'state'=> 'Colorado',
+      'city'=> 'Salida',
+      'streeet_address' => '3485 Mountain Drive',
+      'zip' => '84858'
+    }
+  },
+  {
+    first_name: 'Amanda',
+    last_name: 'Toledo',
+    title: 'Host',
+    email: 'amanda@gmail.com',
+    phone: '720-097-3846',
+    image: 'https://images.unsplash.com/photo-1560365163-3e8d64e762ef?ixid=MnwxMjA3fDB8MHxzZWFyY2h8OTl8fHBlcnNvbnxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60',
+    hire_date: Date.parse('3/12/2016'),
     training: false,
     address: {
       'state'=> 'Colorado',
       'city'=> 'Boulder',
-      'streeet_address' => '6578 Mapleton Ave',
+      'streeet_address' => '2387 28th Street',
       'zip' => '80304'
     }
   },
+  {
+    first_name: 'Tabitha',
+    last_name: 'Johnson',
+    title: 'Bartender',
+    email: 'tabitha@gmail.com',
+    phone: '802-324-2344',
+    image: 'https://images.unsplash.com/photo-1554384645-13eab165c24b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTA0fHxwZXJzb258ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60',
+    hire_date: Date.parse('9/9/2014'),
+    training: false,
+    address: {
+      'state'=> 'Colorado',
+      'city'=> 'Golden',
+      'streeet_address' => '4566 Riverwood Drive',
+      'zip' => '85633'
+    }
+  }
 ])
